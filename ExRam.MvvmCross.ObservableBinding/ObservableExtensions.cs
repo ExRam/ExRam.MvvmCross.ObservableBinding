d07524907836512ca73f92bbe31d6fdc9136aa01
Blewzman <dw@exram.de>
2014-01-28 12:04:41 +0100

    Added ObserveOn(IMvxMainThreadDispatcher) method to allow observation on main thread.

---
+++ ExRam.MvvmCross.ObservableBinding/ObservableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore.Core;
+
+namespace System.Reactive.Linq
+{
+    public static class ObservableExtensions
+    {
+        public static IObservable<T> ObserveOn<T>(this IObservable<T> source, IMvxMainThreadDispatcher dispatcher)
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(dispatcher != null);
+
+            return Observable.Create<T>((observer) => source.Subscribe(
+                (value) => dispatcher.RequestMainThreadAction(() => observer.OnNext(value)),
+                (ex) => dispatcher.RequestMainThreadAction(() => observer.OnError(ex)),
+                () => dispatcher.RequestMainThreadAction(() => observer.OnCompleted())));
+        }
+    }
+}
