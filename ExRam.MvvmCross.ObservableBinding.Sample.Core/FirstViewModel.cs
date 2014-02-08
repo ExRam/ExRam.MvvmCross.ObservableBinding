3b53b9f52fa20b6fed835df718f4649c659c0ff9
Blewzman <dw@exram.de>
2014-02-08 15:41:24 +0100

    Added a sample app.

---
+++ ExRam.MvvmCross.ObservableBinding.Sample.Core/FirstViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace ExRam.MvvmCross.ObservableBinding.Sample.Core
+{
+    public class FirstViewModel : MvxViewModel
+    {
+        public IObservable<string> ElapsedSeconds
+        {
+            get
+            {
+                return Observable
+                    .Interval(TimeSpan.FromSeconds(1))
+                    .Select(x => x.ToString())
+                    .ObserveOn(this.Dispatcher);
+            }
+        }
+    }
+}
