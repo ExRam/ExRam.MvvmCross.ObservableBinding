51711af5c10845dced4026c46beaa1b79c06dc08
Daniel Weber <dw@exram.de>
2015-04-23 17:26:37 +0200

    Add PluginLoader.

---
+++ ExRam.MvvmCross.ObservableBinding/PluginLoader.cs
@@ -0,0 +1,24 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Core;
+using Cirrious.CrossCore.Plugins;
+using Cirrious.MvvmCross.Binding.Bindings.Source.Construction;
+
+namespace ExRam.MvvmCross.ObservableBinding
+{
+    public sealed class PluginLoader
+        : IMvxPluginLoader
+    {
+        public static readonly PluginLoader Instance = new PluginLoader();
+
+        private bool _loaded;
+
+        public void EnsureLoaded()
+        {
+            if (!this._loaded)
+            {
+                Mvx.CallbackWhenRegistered<IMvxSourceBindingFactoryExtensionHost>(host => host.Extensions.Insert(0, new ObservableMvxPropertySourceBindingFactoryExtension(Mvx.Resolve<IMvxMainThreadDispatcher>())));
+                this._loaded = true;
+            }
+        }
+    }
+}
